89a2fa019e5b9b97bb11b71e0ab15ee4b188c33f
LaiQingFa <2511695066@qq.com>
2017-09-30 15:53:02 +0800

    异常处理

--- src/main/java/com/example/ssmspringboot/util/CommonErrorPageRegistry.java
+++ src/main/java/com/example/ssmspringboot/util/CommonErrorPageRegistry.java
@@ -14,8 +14,8 @@ public class CommonErrorPageRegistry implements ErrorPageRegistrar {
 
 
     public void registerErrorPages(ErrorPageRegistry errorPageRegistry) {
-        ErrorPage errorPage404=new ErrorPage(HttpStatus.NOT_FOUND,"/errorPage/404");
-        ErrorPage errorPage500=new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR,"/errorPage/404");
+        ErrorPage errorPage404=new ErrorPage(HttpStatus.NOT_FOUND,"/index");
+        ErrorPage errorPage500=new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR,"/index");
         errorPageRegistry.addErrorPages(errorPage404,errorPage500);
     }
 }
